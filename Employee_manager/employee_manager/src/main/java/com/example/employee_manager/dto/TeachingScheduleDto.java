7835f765133f01b34bf3476a8a04115b8d8ac8e2
huupham131 <140997305+huupham131@users.noreply.github.com>
2023-11-30 15:54:40 +0700

    huu tạo controller và dto

---
+++ Employee_manager/employee_manager/src/main/java/com/example/employee_manager/dto/TeachingScheduleDto.java
@@ -0,0 +1,4 @@
+package com.example.employee_manager.dto;
+
+public class TeachingScheduleDto {
+}
--- Employee_manager/employee_manager/src/main/java/com/example/employee_manager/service/ITeachingScheduleService.java
+++ Employee_manager/employee_manager/src/main/java/com/example/employee_manager/service/ITeachingScheduleService.java
@@ -1,4 +1,14 @@
 package com.example.employee_manager.service;
 
+import com.example.employee_manager.model.TeachingSchedule;
+
+import java.util.List;
+
 public interface ITeachingScheduleService {
+    void save(TeachingSchedule teachingSchedule);
+    void update(TeachingSchedule teachingSchedule);
+    List<TeachingSchedule> getAll();
+    void delete(int id);
+    TeachingSchedule getById(int id);
+
 }
