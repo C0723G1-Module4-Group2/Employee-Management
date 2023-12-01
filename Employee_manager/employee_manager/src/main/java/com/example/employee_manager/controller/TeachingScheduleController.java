39c633be4b4de96a0450655f449522708376c3ee
huupham131 <140997305+huupham131@users.noreply.github.com>
2023-12-01 13:28:34 +0700

    fix bt

--- Employee_manager/employee_manager/src/main/java/com/example/employee_manager/controller/TeachingScheduleController.java
+++ Employee_manager/employee_manager/src/main/java/com/example/employee_manager/controller/TeachingScheduleController.java
@@ -1,4 +1,18 @@
 package com.example.employee_manager.controller;
 
+import com.example.employee_manager.service.ITeachingScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/schedule")
 public class TeachingScheduleController {
+    @Autowired
+    private ITeachingScheduleService teachingScheduleService;
+    @GetMapping("")
+    public String showCalendar(){
+        return "/teaching_schedule/list";
+    }
 }
