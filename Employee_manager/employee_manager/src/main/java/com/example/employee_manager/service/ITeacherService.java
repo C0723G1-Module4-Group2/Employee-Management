675ca156c4eacf0e928571f9eb47dfb055cff45a
unknown <tbin08122004@gmail.com>
2023-12-02 12:03:37 +0700

    tuanUpdateRepo-service-views-controller

---
+++ Employee_manager/employee_manager/src/main/java/com/example/employee_manager/service/ITeacherService.java
@@ -0,0 +1,12 @@
+package com.example.employee_manager.service;
+
+import com.example.employee_manager.model.Teacher;
+
+import java.util.List;
+
+public interface ITeacherService {
+    public void addNewTeacher(Teacher teacher);
+    public void deleteTeacher(int id);
+    public List<Teacher> getAll();
+    public void editTeacher(Teacher teacher);
+}
