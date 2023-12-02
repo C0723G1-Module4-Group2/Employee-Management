f60a8e653532a4d28f119e98467c41fd432f7f04
unknown <tbin08122004@gmail.com>
2023-12-02 12:24:25 +0700

    tuanUpdateRepo-service-views-controller

--- Employee_manager/employee_manager/src/main/java/com/example/employee_manager/controller/TeacherController.java
+++ Employee_manager/employee_manager/src/main/java/com/example/employee_manager/controller/TeacherController.java
@@ -1,10 +1,14 @@
 package com.example.employee_manager.controller;
 
+import com.example.employee_manager.model.Teacher;
 import com.example.employee_manager.service.ITeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller()
 @RequestMapping("/teacher")
@@ -12,7 +16,10 @@ public class TeacherController {
     @Autowired
     private ITeacherService iTeacherService;
     @GetMapping("")
-    public String showManagementTeacher(){
+    public String showManagementTeacher(Model model){
+        List<Teacher> teacherList = iTeacherService.getAll();
+        model.addAttribute("teachers",teacherList);
         return "/teacher/managementTeacher";
     }
+
 }
